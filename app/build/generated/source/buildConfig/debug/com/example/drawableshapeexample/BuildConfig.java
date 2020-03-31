0d0a06614c82db5fee1dfec9d4014c474937af3b
Vlad-Shestenrin <valdon1202@gmail.com>
2020-03-31 13:06:42 +0300

    complite

--- app/build/generated/source/buildConfig/debug/com/example/drawableshapeexample/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/example/drawableshapeexample/BuildConfig.java
@@ -5,7 +5,7 @@ package com.example.drawableshapeexample;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.example.drawableexample";
+  public static final String APPLICATION_ID = "com.example.drawableshapeexample";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
